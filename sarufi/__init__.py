9d7a844d5b80c06b6bd9980013d1957d6e912da6
Kalebu <isaackeinstein@gmail.com>
2022-08-23 15:30:24 +0300

    fixed bugs and added a bit of docstring

--- sarufi/__init__.py
+++ sarufi/__init__.py
@@ -2,6 +2,7 @@ import json
 import logging
 import requests
 from uuid import uuid4
+from typing import Dict, Dict, Any, List, Optional, Union, Tuple, Callable, cast
 
 # Set up logging
 logging.basicConfig(
@@ -12,10 +13,17 @@ logging.basicConfig(
 class Sarufi(object):
     BASE_URL = "https://api.sarufi.io/"
 
-    def __init__(self, username, password):
+    def __init__(self, username=None, password=None, token=None):
         self.username = username
         self.password = password
-        self.token = self.get_token()
+        if token:
+            self.token = token
+        else:
+            self.token = self.get_token()
+
+    @staticmethod
+    def strip_of_nones(data: Dict):
+        return {k: v for k, v in data.items() if v is not None}
 
     @property
     def headers(self):
@@ -43,34 +51,88 @@ class Sarufi(object):
         if self.token.get("token"):
             return True
 
-    def bots(self):
+    def bots(self) -> Union[List[Bot], Dict]:
+        """bots
+
+        Gets all user chatbots from sarufi engine
+
+        Returns:
+            Union[List[Bot], Dict]: List of chatbots if successful otherwise dict with error message
+
+        Examples:
+
+        >>> from sarufi import Sarufi
+        >>> sarufi = Sarufi('myname@domain.com', 'password')
+        2022-08-23 15:03:00,928 - root - INFO - Getting token
+        >>> sarufi.bots()
+        2022-08-23 15:03:57,845 - root - INFO - Getting bots
+        [Bot(id=4, name=iBank, description=PUT DESCRIPTION HERE), Bot(id=5, name=Maria, description=Swahili Cognitive Mental Health Chatbot)]
+
+        """
         logging.info("Getting bots")
         url = self.BASE_URL + "chatbots"
         response = requests.get(url, headers=self.headers)
         if response.status_code == 200:
-            return [Bot(bot) for bot in response.json()]
+            return [Bot(bot, token=self.token) for bot in response.json()]
         return response.json()
 
-    # Not implemented yet
-    def get_bot(self, bot_id):
-        url = self.BASE_URL + "projects/" + str(bot_id)
+    def get_bot(self, id: int) -> Union[Bot, Dict[Any, Any]]:
+        """get_bot
+
+        Gets a chatbot  with a specified (id) from sarufi engine
+
+        Args:
+            id (int): The ID of the chatbot to get
+
+        Returns:
+            Union[Bot, Dict[Any, Any]]: Chatbot object if bot found otherwise dict with error message
+        """
+        url = self.BASE_URL + "chatbot/" + str(id)
         response = requests.get(url, headers=self.headers)
         if response.status_code == 200:
             return Bot(response.json())
         return response.json()
 
     def create_bot(self, name: str, description=None, flow=None, intents=None):
+        """create_bot
+
+        Creates a new chatbot using sarufi engine
+
+        Args:
+            name (str): Name of the chatbot
+            description (_type_, optional): description about what the chabot does. Defaults to None.
+            flow (_type_, optional): Flow to control execution of the chatbot. Defaults to None.
+            intents (_type_, optional): Intents to train the chatbot. Defaults to None.
+
+        Returns:
+            Union[Bot, Dict]: Chatbot object if bot created successfully otherwise dict with error message
+
+        Examples:
+
+        >>> from sarufi import Sarufi
+        >>> sarufi = Sarufi('myname@domain.com', 'password')
+        2022-08-23 15:03:00,928 - root - INFO - Getting token
+        >>> bot = sarufi.create_bot(name='Maria')
+        2022-08-23 15:03:09,891 - root - INFO - Creating bot
+        >>> bot
+        Bot(id=5, name=Maria, description=PUT DESCRIPTION HERE)
+        """
+
         logging.info("Creating bot")
         url = self.BASE_URL + "chatbot"
         data = json.dumps(
-            {
-                "name": name,
-                "description": description,
-                "intents": intents,
-                "flows": flow,
-            }
+            self.strip_of_nones(
+                {
+                    "name": name,
+                    "description": description,
+                    "intents": intents,
+                    "flows": flow,
+                }
+            )
         )
         response = requests.post(url, data=data, headers=self.headers)
+        if response.status_code == 200:
+            return Bot(response.json(), token=self.token)
         return response.json()
 
     def delete_bot(self, id: int):
@@ -85,26 +147,26 @@ class Sarufi(object):
         logging.info("Updating bot")
         url = self.BASE_URL + f"chatbot/{id}"
         data = json.dumps(
-            {
-                "name": name,
-                "description": description,
-                "intents": intents,
-                "flows": flow,
-            }
+            self.strip_of_nones(
+                {
+                    "name": name,
+                    "description": description,
+                    "intents": intents,
+                    "flows": flow,
+                }
+            )
         )
         response = requests.put(url, data=data, headers=self.headers)
+        if response.status_code == 200:
+            return Bot(response.json(), token=self.token)
         return response.json()
 
     def fetch_response(
-        self,
-        bot_id: int,
-        chat_id: str,
-        message: str,
-        message_type: str = "text",
-        channel: str = "general",
+        self, bot_id: int, chat_id: str, message: str, message_type: str, channel: str
     ):
         url = self.BASE_URL + "conversation/"
         if channel == "whatsapp":
+            logging.info("Sending message to bot via whatsapp")
             url = url + "whatsapp/"
 
         data = json.dumps(
@@ -120,8 +182,8 @@ class Sarufi(object):
     def chat(
         self,
         bot_id: int,
-        message: str = "Hello",
         chat_id: str = str(uuid4()),
+        message: str = "Hello",
         message_type: str = "text",
         channel: str = "general",
     ):
@@ -139,7 +201,11 @@ class Sarufi(object):
         """
         logging.info("Sending message to bot and returning response")
         response = self.fetch_response(
-            bot_id=bot_id, chat_id=chat_id, message=message, message_type=message_type
+            bot_id=bot_id,
+            chat_id=chat_id,
+            message=message,
+            message_type=message_type,
+            channel=channel,
         )
         logging.info(f"Status code: {response.status_code}")
         if response.status_code == 200:
@@ -164,14 +230,15 @@ class Sarufi(object):
         return response.json()
 
 
-class Bot(object):
+class Bot(Sarufi):
     """ "
     BOT OBJECT
 
     Helper functions to access bot data in easy way
     """
 
-    def __init__(self, data):
+    def __init__(self, data: Dict, token=None):
+        super().__init__(token=token)
         self.data = data
 
     @property
@@ -182,17 +249,49 @@ class Bot(object):
     def name(self):
         return self.data.get("name")
 
+    @name.setter
+    def name(self, name: str):
+        if isinstance(name, str):
+            self.data["name"] = name
+            return self.update_bot(self.id, name=name)
+        else:
+            raise TypeError("name must be a string")
+
     @property
     def description(self):
         return self.data.get("description")
+
+    @description.setter
+    def description(self, description: str):
+        if isinstance(description, str):
+            self.data["description"] = description
+            return self.update_bot(self.id, description=description)
+        else:
+            raise TypeError("description must be a string")
 
     @property
     def intents(self):
         return self.data.get("intents")
 
+    @intents.setter
+    def intents(self, intents: Dict):
+        if isinstance(intents, dict):
+            self.data["intents"] = intents
+            return self.update_bot(self.id, intents=intents)
+        else:
+            raise TypeError("intents must be a Dictionary")
+
     @property
     def flow(self):
         return self.data.get("flows")
+
+    @flow.setter
+    def flow(self, flow: Dict):
+        if isinstance(flow, dict):
+            self.data["flows"] = flow
+            return self.update_bot(self.id, flow=flow)
+        else:
+            raise TypeError("flow must be a Dictionary")
 
     def __str__(self) -> str:
         return f"Bot(id={self.id}, name={self.name}, description={self.description})"
